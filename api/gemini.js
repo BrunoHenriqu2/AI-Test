daeaccfcf368c216e77808986bd55874c167462a
Bruno Henrique Hoffmann do Nascimento <brunohenriquehoffmann55@gmail.com>
2025-04-19 00:18:32 -0300

    t

--- api/gemini.js
+++ api/gemini.js
@@ -10,7 +10,7 @@ export default async function generateGemini(req, res) {
     }
     
     const {message} = req.body
-    const prompt = `${String(message)} (resuma o máximo possível e faça questão da resposta estar em PT-BR)`;
+    const prompt = `${String(message)} (Se for possível resuma o máximo que puder. Também faça questão da resposta estar em PT-BR)`;
     console.log(message, prompt)
     try {
         const generativeRes = await model.generateContent([prompt]);
